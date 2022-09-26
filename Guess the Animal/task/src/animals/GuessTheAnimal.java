15e38d90352865261f0b65fb80e358fd717a5eed
Serge Fedosov <107842308+serge-fedosov@users.noreply.github.com>
2022-09-26 21:53:29 +0400

    Guess the Animal - Stage 2/6

--- Guess the Animal/task/src/animals/GuessTheAnimal.java
+++ Guess the Animal/task/src/animals/GuessTheAnimal.java
@@ -6,6 +6,7 @@ import java.util.Scanner;
 
 public class GuessTheAnimal {
     static final Random random = new Random();
+    Scanner scanner = new Scanner(System.in);
 
     String[] POSITIVE_RESPONSES = {"y", "yes", "yeah", "yep", "sure", "right", "affirmative", "correct", "indeed",
             "you bet", "exactly", "you said it"};
@@ -17,6 +18,14 @@ public class GuessTheAnimal {
             "Oh, no, don't try to confuse me: say yes or no."};
 
     String[] GOODBYE_TEXT = {"Goodbye", "Bye", "See you later", "See ya!", "Catch you later", "Have a good day", "Bye bye!"};
+
+    private String animal1;
+    private String animal2;
+    private String fact;
+    private String fact2True;
+    private String fact2False;
+    private String fact3;
+    private boolean animal2fact;
 
     public void greeting() {
         int hour = LocalTime.now().getHour();
@@ -38,40 +47,6 @@ public class GuessTheAnimal {
         System.out.println(GOODBYE_TEXT[random.nextInt(GOODBYE_TEXT.length)]);
     }
 
-    public void guessTheAnimal() {
-        Scanner scanner = new Scanner(System.in);
-
-        System.out.println();
-        System.out.println("Enter an animal:");
-
-        String animal = scanner.nextLine().toLowerCase();
-
-        if (animal.startsWith("the ")) {
-            animal = changeArticle(animal);
-        } else if (!animal.startsWith("a ") && !animal.startsWith("an ")) {
-            animal = addArticle(animal);
-        }
-
-        System.out.println("Is it " + animal + "?");
-
-        while (true) {
-            String answer = scanner.nextLine().trim().toLowerCase();
-            if (answer.endsWith(".") || answer.endsWith("!")) {
-                answer = answer.substring(0, answer.length() - 1);
-            }
-
-            if (arrayContainsString(POSITIVE_RESPONSES, answer)) {
-                System.out.println("You answered: Yes");
-                return;
-            } else if (arrayContainsString(NEGATIVE_RESPONSES, answer)) {
-                System.out.println("You answered: No");
-                return;
-            }
-
-            System.out.println(CLARIFY[random.nextInt(CLARIFY.length)]);
-        }
-    }
-
     private boolean arrayContainsString(String[] array, String value) {
         for (var str: array) {
             if (str.equals(value)) {
@@ -89,5 +64,101 @@ public class GuessTheAnimal {
     private String addArticle(String animal) {
         char ch = animal.charAt(0);
         return (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u' || ch == 'y') ? "an " + animal : "a " + animal;
+    }
+
+    private String removeArticle(String animal) {
+        if (animal.startsWith("the ")) {
+            return animal.substring(4);
+        } else if (animal.startsWith("a ")) {
+            return animal.substring(2);
+        } else if (animal.startsWith("an ")) {
+            return animal.substring(3);
+        } else {
+            return animal;
+        }
+    }
+
+    private void setFact(String factString) {
+        if (factString.endsWith(".") || factString.endsWith("?")) {
+            factString = factString.substring(0, factString.length() - 1);
+        }
+
+        if (factString.startsWith("it can ")) {
+            fact = factString.substring(7);
+            fact2True = "can";
+            fact2False = "can't";
+            fact3 = "Can it";
+        } else if (factString.startsWith("it has ")) {
+            fact = factString.substring(7);
+            fact2True = "has";
+            fact2False = "doesn't have";
+            fact3 = "Does it have";
+        } else if (factString.startsWith("it is ")) {
+            fact = factString.substring(6);
+            fact2True = "is";
+            fact2False = "isn't";
+            fact3 = "Is it";
+        } else {
+            fact = null;
+            fact2True = null;
+            fact2False = null;
+            fact3 = null;
+        }
+    }
+
+    public void guessTheAnimal() {
+        System.out.println();
+
+        System.out.println("Enter the first animal:");
+        animal1 = removeArticle(scanner.nextLine().trim().toLowerCase());
+//        if (animal.startsWith("the ")) {
+//            animal = changeArticle(animal);
+//        } else if (!animal.startsWith("a ") && !animal.startsWith("an ")) {
+//            animal = addArticle(animal);
+//        }
+
+        System.out.println("Enter the second animal:");
+        animal2 = removeArticle(scanner.nextLine().trim().toLowerCase());
+
+        do {
+            System.out.printf("Specify a fact that distinguishes %s from %s.\n" +
+                    "The sentence should be of the format: 'It can/has/is ...'.\n\n", addArticle(animal1), addArticle(animal2));
+
+            setFact(scanner.nextLine().trim().toLowerCase());
+
+            if (fact == null) {
+                System.out.println("The examples of a statement:\n" +
+                        " - It can fly\n" +
+                        " - It has horn\n" +
+                        " - It is a mammal");
+            }
+        } while (fact == null);
+
+        System.out.printf("Is it correct for %s?\n", addArticle(animal2));
+
+        while (true) {
+            String answer = scanner.nextLine().trim().toLowerCase();
+            if (answer.endsWith(".") || answer.endsWith("!")) {
+                answer = answer.substring(0, answer.length() - 1);
+            }
+
+            if (arrayContainsString(POSITIVE_RESPONSES, answer)) {
+                animal2fact = true;
+                break;
+            } else if (arrayContainsString(NEGATIVE_RESPONSES, answer)) {
+                animal2fact = false;
+                break;
+            }
+
+            System.out.println(CLARIFY[random.nextInt(CLARIFY.length)]);
+        }
+
+        System.out.println("I learned the following facts about animals:");
+        System.out.printf("- The %s %s %s.\n", animal1, animal2fact ? fact2False : fact2True, fact);
+        System.out.printf("- The %s %s %s.\n", animal2, animal2fact ? fact2True : fact2False, fact);
+
+        System.out.println("I can distinguish these animals by asking the question:");
+
+        System.out.printf("- %s %s?\n", fact3, fact);
     }
 }
